51aa147214edd5fea292e5be73cfa530060e23b7
Arina <makarovaas2008@gmail.com>
2025-01-31 15:40:40 +0300

    добавлено пояснение к коду и 2 поля в таблицу users (логин и хэш пароля)

--- chat/src/main/kotlin/scripts.sql
+++ chat/src/main/kotlin/scripts.sql
@@ -1,6 +1,8 @@
 create table public.users
 (
     id bigserial primary key,
+    login varchar(255) not null unique,
+    password_hash varchar(255),
     last_name  varchar(255) not null,
     first_name varchar(255),
     gender varchar(6), --6 потому что либо male(4), female(6), other(5)- то есть маскимальная длина 6, больше не нужно, так как будет избыточно
@@ -24,3 +26,8 @@ create table public.reactions_type
     emotion_type varchar(255),
     emoji varchar(255)
 )
+
+--база данных состоит из 3 таблиц
+-- таблица users предназначена для хранения данных о пользователях (их id, логин, хэш пароля, имя, фамилия, возраст и пол)
+--таблица reactions создана для того, чтобы хранить данные о реакциях (кто отправил, кому отправил - соответственно эти поля связаны с таблицей users)
+-- таблица reactions_type создана для того, чтобы хранить информацию о реакциях (какая конкретно реакция, то есть ее тип)
